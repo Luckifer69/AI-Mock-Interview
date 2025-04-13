3ce85fc2f5e99c97a71acedcc9d1871f898b2615
Luckifer69 <divyanshukr.1410@gmail.com>
2025-04-13 20:58:24 +0530

    adding file

---
+++ drizzle.config.js
@@ -0,0 +1,8 @@
+/** @type {import("dirzzle-kit").Config} */
+export default {
+    schema: "./utils/schema.js",
+    dialect: 'postgresql',
+    dbCredentials:{
+        url: 'postgresql://neondb_owner:npg_stW2ApI7OwFj@ep-morning-mud-a5k9wjgq-pooler.us-east-2.aws.neon.tech/neondb?sslmode=require',
+    }
+};
